1e7bfcc412985acdbaf47bc99e06cd483dd4d68c
AlexNedev <71048559+AlexNedev@users.noreply.github.com>
2020-09-09 20:28:41 -0400

    Add name 'Alex Nedev' to GitHub Demo

--- assets/contributors.js
+++ assets/contributors.js
@@ -6,5 +6,9 @@ export const contributors = [
   {
     name: 'cuHacking Example',
     githubProfile: 'https://github.com/cuHacking'
+  },
+  {
+   name: 'Alex Nedev',
+   gitHubProfile: 'https://github.com/AlexNedev'
   }
 ]
