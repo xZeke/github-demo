a7a05afc7052078e16c2ffa51aa0147831b860c8
Donaldlau8 <71048547+Donaldlau8@users.noreply.github.com>
2020-09-09 20:30:44 -0400

    Add my name and GitHub Profile

--- assets/contributors.js
+++ assets/contributors.js
@@ -6,5 +6,9 @@ export const contributors = [
   {
     name: 'cuHacking Example',
     githubProfile: 'https://github.com/cuHacking'
+  },
+  {
+    name: 'Donaldlau8',
+    githubProfile: 'https://github.com/Donaldlau8'
   }
 ]
