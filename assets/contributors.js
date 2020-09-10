126ee4c417faca3cfc722450de7cb933d7bdb95d
Reszonance <66890102+Reszonance@users.noreply.github.com>
2020-09-09 21:10:56 -0400

    Add name and Github profile

--- assets/contributors.js
+++ assets/contributors.js
@@ -2,5 +2,9 @@ export const contributors = [
   {
     name: 'Wal Wal',
     githubProfile: 'https://github.com/Walsker'
+  },
+  {
+    name: 'Reszonance',
+    githubProfile: 'https://github.com/Reszonance'
   }
 ]
