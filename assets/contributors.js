69cc801fe04311b37ff63b23ec3ee1adb7d104f4
Q <24325286+saturnrobot@users.noreply.github.com>
2020-09-10 00:26:28 +0000

    added my name to the project
    my name is "Quentin Wolkensperg" I also added my nick name as well.

--- assets/contributors.js
+++ assets/contributors.js
@@ -4,6 +4,10 @@ export const contributors = [
     githubProfile: 'https://github.com/Walsker'
   },
   {
+    name: 'Quentin (Q) Wolkensperg',
+    githubProfile: 'https://github.com/saturnrobot'
+  },
+  {
     name: 'cuHacking Example',
     githubProfile: 'https://github.com/cuHacking'
   }
