42fb5ac6b6edd34cbdc94c2a84fe23023fdf004e
Eren Sulutas <erensulu11@gmail.com>
2020-09-09 20:26:04 -0400

    Added name and gh
    sample text

--- assets/contributors.js
+++ assets/contributors.js
@@ -6,5 +6,9 @@ export const contributors = [
   {
     name: 'cuHacking Example',
     githubProfile: 'https://github.com/cuHacking'
+  },
+  {
+    name: 'Eren Sulutas',
+    githubProfile: 'https://github.com/esulu'
   }
 ]
