a61af10d987710278f288836e3df282701a43773
Siddharth N <siddharthnatamai@gmail.com>
2020-09-09 20:10:56 -0400

    Update contributors.js

--- assets/contributors.js
+++ assets/contributors.js
@@ -6,5 +6,9 @@ export const contributors = [
   {
     name: 'cuHacking Example',
     githubProfile: 'https://github.com/cuHacking'
+  },
+  {
+    name: 'Siddharth Natamai',
+    githubProfile: 'https://github.com/sidbmw/'
   }
 ]
