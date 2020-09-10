256be043bea47ba54715c575415c1821fa35a00b
bardia-p <59774562+bardia-p@users.noreply.github.com>
2020-09-09 20:28:19 -0400

    Add my name and github profile
    cuhacking github demo

--- assets/contributors.js
+++ assets/contributors.js
@@ -6,5 +6,9 @@ export const contributors = [
   {
     name: 'cuHacking Example',
     githubProfile: 'https://github.com/cuHacking'
+  },
+  {
+    name: 'Bardia Parmoun',
+    githubProfile: 'https://github.com/bardia-p'
   }
 ]
