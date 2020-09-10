203097e857ecd2870a5d4e030b2e46309cb2954b
SolidSoundGuy <71048544+SolidSoundGuy@users.noreply.github.com>
2020-09-09 21:26:55 -0400

    Update contributors.js (#26)

--- assets/contributors.js
+++ assets/contributors.js
@@ -3,6 +3,10 @@ export const contributors = [
     name: 'Wal Wal',
     githubProfile: 'https://github.com/Walsker'
   },
+   {
+    name: 'AutoScott',
+    githubProfile: 'https://github.com/SolidSoundGuy'
+  },
   {
     name: 'Jess Tiberio',
     githubProfile: 'https://github.com/JTibs18'
