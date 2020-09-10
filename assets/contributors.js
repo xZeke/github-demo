1ce94e73285643ab8d58929c1460eb723449d6e1
xZeke <71048449+xZeke@users.noreply.github.com>
2020-09-09 21:57:54 -0400

    Update contributors.js

--- assets/contributors.js
+++ assets/contributors.js
@@ -6,7 +6,7 @@ export const contributors = [
   {
     name: 'cuHacking Example',
     githubProfile: 'https://github.com/cuHacking'
-  }
+  },
   {
     name: 'Jacob Zekorn',
     githubProfile: 'https://github.com/xZeke'
